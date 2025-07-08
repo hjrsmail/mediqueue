b637454db71a6c40916c62ace3b7be9a71bd81f3
Hijir Ismail <hijir512@gmail.com>
2025-07-08 13:04:47 +0800

    feat: add Navbar component with navigation and user menu functionality

---
+++ components/dashboard/Navbar.tsx
@@ -0,0 +1,267 @@
+"use client"
+
+import * as React from "react"
+import { ChevronRight, type LucideIcon } from "lucide-react"
+import Link from "next/link"
+import { signOut } from "next-auth/react"
+
+
+
+import {
+    Collapsible,
+    CollapsibleContent,
+    CollapsibleTrigger,
+} from "@/components/ui/collapsible"
+import {
+    SidebarGroup,
+    SidebarGroupLabel,
+    SidebarMenu,
+    SidebarMenuAction,
+    SidebarGroupContent,
+    SidebarMenuButton,
+    SidebarMenuItem,
+    SidebarMenuSub,
+    SidebarMenuSubButton,
+    SidebarMenuSubItem,
+    useSidebar,
+} from "@/components/ui/sidebar"
+
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuLabel,
+    DropdownMenuItem,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+    DropdownMenuGroup,
+} from "@/components/ui/dropdown-menu"
+
+
+import {
+    BadgeCheck,
+    Bell,
+    ChevronsUpDown,
+    LogOut,
+} from "lucide-react"
+
+import {
+    Avatar,
+    AvatarFallback,
+    AvatarImage,
+} from "@/components/ui/avatar"
+
+
+
+function NavMain({
+    items,
+}: {
+    items: {
+        title: string
+        url: string
+        icon: LucideIcon
+        isActive?: boolean
+        items?: {
+            title: string
+            url: string
+        }[]
+    }[]
+}) {
+    return (
+        <SidebarGroup>
+            <SidebarGroupLabel>Setting</SidebarGroupLabel>
+            <SidebarMenu>
+                {items.map((item) => (
+                    <Collapsible key={item.title} asChild defaultOpen={item.isActive}>
+                        <SidebarMenuItem>
+                            <SidebarMenuButton asChild tooltip={item.title}>
+                                <a href={item.url}>
+                                    <item.icon />
+                                    <span>{item.title}</span>
+                                </a>
+                            </SidebarMenuButton>
+                            {item.items?.length ? (
+                                <>
+                                    <CollapsibleTrigger asChild>
+                                        <SidebarMenuAction className="data-[state=open]:rotate-90">
+                                            <ChevronRight />
+                                            <span className="sr-only">Toggle</span>
+                                        </SidebarMenuAction>
+                                    </CollapsibleTrigger>
+                                    <CollapsibleContent>
+                                        <SidebarMenuSub>
+                                            {item.items?.map((subItem) => (
+                                                <SidebarMenuSubItem key={subItem.title}>
+                                                    <SidebarMenuSubButton asChild>
+                                                        <a href={subItem.url}>
+                                                            <span>{subItem.title}</span>
+                                                        </a>
+                                                    </SidebarMenuSubButton>
+                                                </SidebarMenuSubItem>
+                                            ))}
+                                        </SidebarMenuSub>
+                                    </CollapsibleContent>
+                                </>
+                            ) : null}
+                        </SidebarMenuItem>
+                    </Collapsible>
+                ))}
+            </SidebarMenu>
+        </SidebarGroup>
+    )
+}
+
+export function NavProjects({
+    projects,
+}: {
+    projects: {
+        name: string
+        url: string
+        icon: React.ElementType
+    }[]
+}) {
+    const { isMobile } = useSidebar()
+
+    return (
+        <SidebarGroup className="group-data-[collapsible=icon]:hidden">
+            {/* <SidebarGroupLabel>Projects</SidebarGroupLabel> */}
+            <SidebarMenu>
+                {projects.map((item) => (
+                    <SidebarMenuItem key={item.name}>
+                        <SidebarMenuButton asChild>
+                            <Link href={item.url}>
+                                <item.icon />
+                                <span>{item.name}</span>
+                            </Link>
+                        </SidebarMenuButton>
+                        <DropdownMenu>
+                            <DropdownMenuTrigger asChild>
+                                <SidebarMenuAction showOnHover>
+                                    {/* <MoreHorizontal /> */}
+                                    <span className="sr-only">More</span>
+                                </SidebarMenuAction>
+                            </DropdownMenuTrigger>
+                        </DropdownMenu>
+                    </SidebarMenuItem>
+                ))}
+            </SidebarMenu>
+        </SidebarGroup>
+    )
+}
+
+export function NavSecondary({
+    items,
+    ...props
+}: {
+    items: {
+        title: string
+        url: string
+        icon: LucideIcon
+    }[]
+} & React.ComponentPropsWithoutRef<typeof SidebarGroup>) {
+    return (
+        <SidebarGroup {...props}>
+            <SidebarGroupContent>
+                <SidebarMenu>
+                    {items.map((item) => (
+                        <SidebarMenuItem key={item.title}>
+                            <SidebarMenuButton asChild size="default">
+                                <Link href={item.url}>
+                                    <item.icon />
+                                    <span>{item.title}</span>
+                                </Link>
+                            </SidebarMenuButton>
+                        </SidebarMenuItem>
+                    ))}
+                </SidebarMenu>
+            </SidebarGroupContent>
+        </SidebarGroup>
+    )
+}
+
+export function NavUser({
+    user,
+}: {
+    user: {
+        name: string
+        email: string
+        avatar?: string
+    }
+}) {
+    const { isMobile } = useSidebar()
+
+    return (
+        <SidebarMenu>
+            <SidebarMenuItem>
+                <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        <SidebarMenuButton
+                            size="lg"
+                            className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+                        >
+                            <Avatar className="h-8 w-8 rounded-lg">
+                                <AvatarImage src={user.avatar} alt={user.name} />
+                                <AvatarFallback className="rounded-lg">CN</AvatarFallback>
+                            </Avatar>
+                            <div className="grid flex-1 text-left text-sm leading-tight">
+                                <span className="truncate font-medium">{user.name}</span>
+                                <span className="truncate text-xs">{user.email}</span>
+                            </div>
+                            <ChevronsUpDown className="ml-auto size-4" />
+                        </SidebarMenuButton>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent
+                        className="w-(--radix-dropdown-menu-trigger-width) min-w-56 rounded-lg"
+                        side={isMobile ? "bottom" : "right"}
+                        align="end"
+                        sideOffset={4}
+                    >
+                        <DropdownMenuLabel className="p-0 font-normal">
+                            <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
+                                <Avatar className="h-8 w-8 rounded-lg">
+                                    <AvatarImage src={user.avatar} alt={user.name} />
+                                    <AvatarFallback className="rounded-lg">CN</AvatarFallback>
+                                </Avatar>
+                                <div className="grid flex-1 text-left text-sm leading-tight">
+                                    <span className="truncate font-medium">{user.name}</span>
+                                    <span className="truncate text-xs">{user.email}</span>
+                                </div>
+                            </div>
+                        </DropdownMenuLabel>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuGroup>
+                            <DropdownMenuItem>
+                                <BadgeCheck />
+                                Account
+                            </DropdownMenuItem>
+                            <DropdownMenuItem>
+                                <Bell />
+                                Notifications
+                            </DropdownMenuItem>
+                        </DropdownMenuGroup>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuItem asChild>
+                            <button
+                                type="button"
+                                onClick={() => signOut({ callbackUrl: "/login" })}
+                                className="w-full text-left flex items-center gap-2"
+                            >
+                                <LogOut />
+                                <span>Log out</span>
+                            </button>
+                        </DropdownMenuItem>
+
+                    </DropdownMenuContent>
+                </DropdownMenu>
+            </SidebarMenuItem>
+        </SidebarMenu>
+    )
+}
+
+export default {
+    NavMain,
+    NavProjects,
+    NavSecondary,
+    NavUser
+}
+
+
