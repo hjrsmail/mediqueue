28ad5ce72d96f8e5fab6b57c22ada16e3569889b
Hijir Ismail <hijir512@gmail.com>
2025-07-14 09:54:36 +0800

    chore(queue): remove dynamic rendering from bukti-antrian page

--- app/(queue)/bukti-antrian/[id]/page.tsx
+++ app/(queue)/bukti-antrian/[id]/page.tsx
@@ -10,8 +10,6 @@ import { notFound } from "next/navigation";
 //     };
 // }
 
-export const dynamic = 'force-dynamic';
-
 export default async function BuktiAntrian({ params }: { params: { id: string; }; }) {
 
     const { id } = params;
